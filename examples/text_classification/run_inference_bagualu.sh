60dc06ed08c26603559346610ec076de942ed6fa
jiangyuzhen <815754845@qq.com>
2022-12-02 17:39:15 +0800

    add: 添加bagualu classification 启动脚本example

---
+++ examples/text_classification/run_inference_bagualu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WORK_DIR=$(dirname $(dirname $(dirname $(readlink -f "$0"))))
+echo "working directory: $WORK_DIR"
+
+cd $WORK_DIR
+
+TASK_DIR=$WORK_DIR/tasks/text_classification_example
+
+if [ ! -d $TASK_DIR ]; then
+    echo "task dir $TASK_DIR not exists, please train first."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=7 
+python gts_engine/gts_engine_inference.py \
+    --task_dir=$TASK_DIR \
+    --engine_type=bagualu \
+    --task_type=classification \
+    --input_path=examples/text_classification/tnews_test.json \
+    --output_path=tnews_output.json
---
+++ examples/text_classification/run_train_bagualu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+WORK_DIR=$(dirname $(dirname $(dirname $(readlink -f "$0"))))
+echo "working directory: $WORK_DIR"
+
+cd $WORK_DIR
+mkdir -p $WORK_DIR/tasks
+mkdir -p $WORK_DIR/pretrained
+
+PRETRAINED_DIR=$WORK_DIR/pretrained
+TASK_DIR=$WORK_DIR/tasks/text_classification_example
+mkdir -p $TASK_DIR
+
+export CUDA_VISIBLE_DEVICES=3 
+python gts_engine/gts_engine_train.py \
+    --engine_type=bagualu \
+    --train_mode=standard \
+    --task_dir=$TASK_DIR \
+    --task_type=classification \
+    --train_data=tnews_train.json \
+    --valid_data=tnews_val.json \
+    --test_data=tnews_test.json \
+    --label_data=tnews_label.json \
+    --data_dir=$WORK_DIR/examples/text_classification \
+    --save_path=$TASK_DIR/outputs \
+    --pretrained_model_dir=$PRETRAINED_DIR \
+    --train_batchsize=2 \
+    --valid_batchsize=4 \
+    --max_len=512 \
+    --max_epochs=1 \
+    --min_epochs=1 \
+    --seed=123
