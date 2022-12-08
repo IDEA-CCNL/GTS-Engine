4a547b339b801c08ae3f7f5f5d024ba44d32c8a4
Yuzhen Jiang <83504734+Deruck@users.noreply.github.com>
2022-12-08 15:51:20 +0800

    Dev jiangyuzhen add namespace to args (#35)
    * update: add .gitignore rules
    
    * add: add specified namespace to args

---
+++ gts_engine/gts_common/arguments.py
@@ -0,0 +1,48 @@
+from typing import Literal, Optional
+from pathlib import Path
+from argparse import Namespace
+
+
+PathStr = str
+FileName = str
+
+class GtsEngineArgs(Namespace):
+    """GTS-Engine参数namespace"""
+    engine_type: Literal["qiankunding", "bagualu"]
+    train_mode: Literal["fast", "standard", "advanced"]
+    task_dir: PathStr
+    task_type: Literal["classification", "similarity", "nli"]
+    num_workers: int
+    train_batchsize: int
+    valid_batchsize: int
+    test_batchsize: int
+    max_len: int
+    pretrained_model_dir: PathStr
+    data_dir: PathStr
+    train_data: FileName
+    valid_data: FileName
+    test_data: FileName
+    label_data: FileName
+    save_path: PathStr
+    seed: int
+    lr: float
+    gpus: int
+    num_sanity_val_steps: int
+    accumulate_grad_batches: int
+    val_check_interval: float
+    
+    @property
+    def train_data_path(self) -> Path:
+        return Path(self.data_dir) / self.train_data
+    
+    @property
+    def valid_data_path(self) -> Path:
+        return Path(self.data_dir) / self.valid_data
+    
+    @property
+    def test_data_path(self) -> Optional[Path]:
+        return Path(self.data_dir) / self.test_data if hasattr(self, "test_data") else None
+    
+    @property
+    def label_data_path(self) -> Optional[Path]:
+        return Path(self.data_dir) / self.label_data if hasattr(self, "label_data") else None
