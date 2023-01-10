5b90b709b044db2a5921db24f472085d6c7c52a5
jiangyuzhen <815754845@qq.com>
2023-01-10 12:22:02 +0800

    update: 完成lib.framework.base_arguments docstring

--- gts_engine/bagualu/lib/framework/__init__.py
+++ gts_engine/bagualu/lib/framework/__init__.py
@@ -2,3 +2,11 @@ from .base_arguments import BaseArguments
 from .base_training_pipeline import BaseTrainingPipeline
 from .base_inference_manager import BaseInferenceManager
 from .base_gts_engine_interface import GtsEngineArgs, BaseGtsEngineInterface
+
+__all__ = [
+    "BaseArguments",
+    "BaseTrainingPipeline",
+    "BaseInferenceManager",
+    "GtsEngineArgs",
+    "BaseGtsEngineInterface"
+]
