1dde14d08c47a29653cce16672f17ab9c81c0f3a
zhubohan <1285603132@qq.com>
2023-01-31 17:58:53 +0800

    label_detection final

--- gts_engine/preprocess/label_detection.py
+++ gts_engine/preprocess/label_detection.py
@@ -27,12 +27,12 @@ logger = Logger().get_log()
 
 def read_data(label_path, data_path):
     input_data = []
-    with open(os.path.join(data_path), encoding='utf8') as f:
+    with open(os.path.join(data_path), encoding="utf8") as f:
         for line in f:
             input_data.append(json.loads(line.strip()))
 
     input_labels = object
-    with open(os.path.join(label_path), encoding='utf8') as f:
+    with open(os.path.join(label_path), encoding="utf8") as f:
         input_labels = json.load(f)
     return input_data, input_labels
 
@@ -48,20 +48,19 @@ def take_sample_input(
     sample_input_data = []
     label_to_count = {}
     for data in input_data:
-        label_to_count[data['id']] = label_to_count.get(data['id'], 0) + 1
-        if label_to_count[data['id']] <= sample_count_every_class:
+        label_to_count[data["id"]] = label_to_count.get(data["id"], 0) + 1
+        if label_to_count[data["id"]] <= sample_count_every_class:
             sample_input_data.append(data)
     return sample_input_data
 
 
 def take_sample(sample_input_data, current_label):
-
-    #adaadad
-    total_sentences = [i['content'] for i in sample_input_data]
+    # 样本采样  1:2 正负样本
+    total_sentences = [i["content"] for i in sample_input_data]
     total_val_data = sample_input_data
     total_val_data_index = [i for i in range(len(total_val_data))]
     true_label_index = [
-        i for i, k in enumerate(total_val_data) if k['label'] == current_label
+        i for i, k in enumerate(total_val_data) if k["label"] == current_label
     ]
 
     tmp_set = set(total_val_data_index).difference(set(true_label_index))
@@ -86,13 +85,15 @@ def predict_process(two_choices, tokenizer, sentences, model):
         tmp_sample = {"content": sentence, "label": two_choices[0]}
         samples.append(tmp_sample)
 
-    train_data = TaskDatasetUnifiedMC(data_path=None,
-                                      args=args,
-                                      used_mask=False,
-                                      tokenizer=tokenizer,
-                                      load_from_list=True,
-                                      samples=samples,
-                                      choice=two_choices)
+    train_data = TaskDatasetUnifiedMC(
+        data_path=None,
+        args=args,
+        used_mask=False,
+        tokenizer=tokenizer,
+        load_from_list=True,
+        samples=samples,
+        choice=two_choices,
+    )
 
     train_dataloader = DataLoader(train_data,
                                   shuffle=False,
@@ -104,14 +105,14 @@ def predict_process(two_choices, tokenizer, sentences, model):
     for batch in tqdm(train_dataloader):
 
         inputs = {
-            'input_ids': batch['input_ids'].cuda(),
-            'attention_mask': batch['attention_mask'].cuda(),
-            'token_type_ids': batch['token_type_ids'].cuda(),
-            "position_ids": batch['position_ids'].cuda(),
-            "mlmlabels": batch['mlmlabels'].cuda(),
-            "clslabels": batch['clslabels'].cuda(),
-            "clslabels_mask": batch['clslabels_mask'].cuda(),
-            "mlmlabels_mask": batch['mlmlabels_mask'].cuda(),
+            "input_ids": batch["input_ids"].cuda(),
+            "attention_mask": batch["attention_mask"].cuda(),
+            "token_type_ids": batch["token_type_ids"].cuda(),
+            "position_ids": batch["position_ids"].cuda(),
+            "mlmlabels": batch["mlmlabels"].cuda(),
+            "clslabels": batch["clslabels"].cuda(),
+            "clslabels_mask": batch["clslabels_mask"].cuda(),
+            "mlmlabels_mask": batch["mlmlabels_mask"].cuda(),
         }
 
         loss, logits, cls_logits, hidden_states = model(**inputs)
@@ -134,11 +135,14 @@ def label_detection(label_path, data_path):
     pretrained_model_dir = os.path.join(
         os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-        "pretrained")
-    download_model_from_huggingface(pretrained_model_dir,
-                                    model_name,
-                                    model_class=MegatronBertForMaskedLM,
-                                    tokenizer_class=BertTokenizer)
+        "pretrained",
+    )
+    download_model_from_huggingface(
+        pretrained_model_dir,
+        model_name,
+        model_class=MegatronBertForMaskedLM,
+        tokenizer_class=BertTokenizer,
+    )
     model_path = os.path.join(pretrained_model_dir, model_name)
     starttime = datetime.now()
     tokenizer = BertTokenizer.from_pretrained(model_path)
@@ -148,7 +152,7 @@ def label_detection(label_path, data_path):
     random.seed(123)
     labels_info = {}
     result_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'label_detection_result.json')
+                               "label_detection_result.json")
     input_data, input_labels = read_data(label_path=label_path,
                                          data_path=data_path)
     sample_input_data = take_sample_input(len(input_labels["labels"]),
@@ -169,26 +173,28 @@ def label_detection(label_path, data_path):
         random_f1 = -1
         label_f1 = -1
         two_choices = [
-            '[MASK]' * len(current_label), '[MASK]' * len(current_label)
+            "[MASK]" * len(current_label), "[MASK]" * len(current_label)
         ]
         n = 0
         while n < 2:
-            total_predicts = predict_process(two_choices=two_choices,
-                                             tokenizer=tokenizer,
-                                             sentences=sentences,
-                                             model=model)
+            total_predicts = predict_process(
+                two_choices=two_choices,
+                tokenizer=tokenizer,
+                sentences=sentences,
+                model=model,
+            )
 
             y_true = []
             y_pred = []
             y_senetence = []
             for sample, predict in zip(val_data, total_predicts):
-                if sample['label'] == current_label:
+                if sample["label"] == current_label:
                     y_true.append(0)
                 else:
                     y_true.append(1)
                 y_pred.append(predict)
                 if predict == 0:
-                    y_senetence.append(sample['content'])
+                    y_senetence.append(sample["content"])
 
             trueY = np.array(y_true)
             testY = np.array(y_pred)
@@ -241,9 +247,12 @@ def label_detection(label_path, data_path):
     for label_value in input_labels["labels"]:
         if label_value in labels_info:
             current_label = label_value
-            tmp_result.append(
-                (current_label, labels_info[current_label][0],
-                 labels_info[current_label][1], labels_info[current_label][2]))
+            tmp_result.append((
+                current_label,
+                labels_info[current_label][0],
+                labels_info[current_label][1],
+                labels_info[current_label][2],
+            ))
         index += 1
     tmp_result.sort(key=lambda x: (x[1], x[3]))
 
@@ -260,15 +269,17 @@ def label_detection(label_path, data_path):
     logger.info(f"result {result}")
     endtime = datetime.now()
     logger.info("RunTime: {}h-{}m-{}s".format(
-        endtime.hour - starttime.hour, endtime.minute - starttime.minute,
-        endtime.second - starttime.second))
+        endtime.hour - starttime.hour,
+        endtime.minute - starttime.minute,
+        endtime.second - starttime.second,
+    ))
 
-    with open(result_path, 'w', encoding='utf-8') as f:
+    with open(result_path, "w", encoding="utf-8") as f:
         for i in result:
-            f.write(json.dumps(i, ensure_ascii=False) + '\n')
+            f.write(json.dumps(i, ensure_ascii=False) + "\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     total_parser = argparse.ArgumentParser()
     total_parser.add_argument("--label_path", type=str, help="data path")
     total_parser.add_argument("--data_path", type=str, help="data path")
